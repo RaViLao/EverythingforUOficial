84f9f480a847ccded4400e2535addb7d48ce6b8f
RaViLao <ramiro.villena@usil.pe>
2017-06-05 01:38:28 -0500

    Conexión de algunos links
    Ahora la página principal se conecta con las demás secciones d ela página.
    Ramiro Villena Lao

--- Pagina Principal/query.js
+++ Pagina Principal/query.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	
 	//esto es para el primer ejemplo
 		$(".elemento").click(function(){
-			alert("no soy boton");
+			var a = 'hola';
+			var ram = $(".elemento").atrr("id");
+			alert(ram);
+			$("#"+ram).css("border", "1px solid red");
+			
 		});	
 });
