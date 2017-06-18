ea664ecfe6eb4e9d880b51d0808a8599222581b3
RaViLao <ramiro.villena@usil.pe>
2017-06-18 00:45:17 -0500

    Mejora de la Aminación de Presentacion
    Código más eficiente para la presentación de imagenes e implementación de los produtos- Ramiro Villena

--- javaS/Eventos.js
+++ javaS/Eventos.js
@@ -4,17 +4,39 @@ $(document).ready(function(){
 		var Images = new Array(
 			'images/universitarios.png',
 			'images/universitarios2.png',
-			'images/universitarios3.png');
+			'images/universitarios3.png',
+			'http://tusimagenesde.com/wp-content/uploads/2016/07/universitarios-1.jpg');
+		var Aux=[];
 			cargar();
 		
 		function cargar(){
-			var index=Math.floor((Math.random()*Images.length));
-			document.getElementById("pres").src=Images[index];
-		}
+			if($("#pres").attr("src")=="" | $("#pres").attr("src")==Aux[0]){
+				$("#pres").attr("src", Images[0]);
+				var mostrado=Images[0];
+				Images.shift();
+				Aux.push(mostrado);
+
+				/*document.getElementById("pres").src=Images[0];*/
+			}
+			else{			
+				
+				if(Images.length==0){
+					while(Aux.length>0){
+						Images.push(Aux.shift());
+					}
+				}else{
+					document.getElementById("pres").src=Images[0];
+					var mos = Images.shift();
+					Aux.push(mos);
+					
+				}
+				
+				}
+			}
+			
 			
 		setInterval(function(){			
 			cargar();
-			document.getElementById("pres").src=Images[index];
 			}, 5000
 			);			
 	});
